3582ec8e3d0575a88ead83abe53e1c10de960f9f
Ben <ben.williams@noaa.gov>
2022-05-06 08:00:04 -0800

    init

---
+++ R/helper.R
@@ -0,0 +1,13 @@
+library(tidyverse)
+library(lubridate)
+library(mgcViz)
+library(funcr) # devtools::install_github("ben-williams/FNGr")
+library(mgcv)
+library(readxl)
+library(ggridges)
+library(EnvStats)
+theme_set(theme_report())
+library(PBSmapping)
+data("nepacLL")
+nepacLL %>% 
+  dplyr::select(group=PID, POS=POS, long=X, lat=Y) -> ak_map
---
+++ data/README.md
@@ -0,0 +1,4 @@
+There are two data files:  
+Contact melissa.head@noaa.gov for the west coast data  
+and   
+cara.rodgveller@noaa.gov for the alaska histological data 
